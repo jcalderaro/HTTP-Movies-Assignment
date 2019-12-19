51480268e4ad8a769d03aa1a62a431d66d4db3aa
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-12-19 12:26:02 -0800

    Phase 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,83 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
+//-----------------------------------------------------------//
+import React, { useState, useEffect } from "react";
+import { Route, withRouter } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import axios from "axios";
+//-----------------------------------------------------------//
 
-const App = () => {
+import UpdateMovie from "./Movies/UpdateMovie";
+
+//-----------------------------------------------------------//
+
+const App = (props) => {
   const [savedList, setSavedList] = useState([]);
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:5000/api/movies")
+    .then(res => console.log("Get Response App", res.data) || setMovies(res.data))
+    .catch(err => console.log("Get Error App", err.response));
+
+  }, [])
+
+//-----------------------------------------------------------//
+
+  const editMovie = (event, movie) => {
+    event.preventDefault()
+    console.log("Edit Movie", movie)
+    const updatedList = movies.map(mv => {
+      if (mv.id === movie.id){
+        return movie;
+      } else {
+        return mv;
+      }
+    })
+    setMovies(updatedList)
+    props.history.push('/')
+  }
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
+//-----------------------------------------------------------//
+
   return (
     <>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <SavedList 
+      list={savedList} 
+      />
+      <Route 
+      exact path="/" 
+      render={props => (
+        <MovieList {...props} movies={movies} />
+      )}
+      />
+
+      {/* \- Add a route at the path `/update-movie/:id` */}
+      <Route 
+      path="/update-movie/:id"
+      render={props => (
+        <UpdateMovie {...props} movies={movies} editMovie={editMovie}/>
+      )}
+      />
+
       <Route
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
+    
       />
     </>
   );
 };
 
-export default App;
+//-----------------------------------------------------------//
+
+export default withRouter(App);
+
+//-----------------------------------------------------------//
